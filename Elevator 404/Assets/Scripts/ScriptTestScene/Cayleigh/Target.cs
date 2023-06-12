d56f998b0cbcfcd01efa08de11de0405d9bd1b29
CerberusCR <124683059+CerberusCR@users.noreply.github.com>
2023-06-12 13:55:16 +0200

    random stuff UwU

--- Elevator 404/Assets/Scripts/ScriptTestScene/Cayleigh/Target.cs
+++ Elevator 404/Assets/Scripts/ScriptTestScene/Cayleigh/Target.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 69f;
+    public GameObject confetti;
 
     public void Damage (float amount)
     {
         health -= amount;
         if (health <= 0f)
         {
+            GameObject deathConfetti = Instantiate(confetti,transform.position, Quaternion.identity);
+            Destroy(deathConfetti, 1); //Destroys it after 1 seconds.
             Destroy(gameObject);
         }
     }

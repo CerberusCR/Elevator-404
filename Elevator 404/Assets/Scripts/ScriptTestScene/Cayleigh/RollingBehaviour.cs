64d132b493a93411c2e7d6803fd06b7f4238fd37
CerberusCR <124683059+CerberusCR@users.noreply.github.com>
2023-06-13 10:53:16 +0200

    DogAI en kermis balloonstand

--- Elevator 404/Assets/Scripts/ScriptTestScene/Cayleigh/RollingBehaviour.cs
+++ Elevator 404/Assets/Scripts/ScriptTestScene/Cayleigh/RollingBehaviour.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleBehaviour : StateMachineBehaviour
+public class RollingBehaviour : StateMachineBehaviour
 {
     float timer;
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -15,21 +16,21 @@ public class IdleBehaviour : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer += Time.deltaTime;
-        if (timer > 600)       //na 5 seconden gaat parameters "isRolling" naar loop animatie inplaats van idle
-            animator.SetBool("isRolling", true);
+        if (timer > 10) ;      
+            ////animator.SetBool("isRolling", false);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+      
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
-    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
     //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)

b9566723f7b1d9307a477a90a7f45ef4cb689ae5
CerberusCR <124683059+CerberusCR@users.noreply.github.com>
2023-07-07 13:28:36 +0200

    Intro comic

--- Elevator 404/Assets/Scenes/Filmpje begin/NextScreen.cs
+++ Elevator 404/Assets/Scenes/Filmpje begin/NextScreen.cs
@@ -8,12 +8,36 @@ public class NextScreen : MonoBehaviour
     public GameObject knop1;
     public GameObject plaatje2;
     public GameObject knop2;
-    public void GoToNextScreen()
+    public GameObject plaatje3;
+    public GameObject knop3;
+    public GameObject plaatje4;
+    public GameObject knop4;
+
+
+    public void GoToNextScreen1To2()
     {
         plaatje1.SetActive(false);
         knop1.SetActive(false);
 
         plaatje2.SetActive(true);
         knop2.SetActive(true);
+    }
+
+    public void GoToNextScreen2To3()
+    {
+        plaatje2.SetActive(false);
+        knop2.SetActive(false);
+
+        plaatje3.SetActive(true);
+        knop3.SetActive(true);
+    }
+
+    public void GoToNextScreen3To4()
+    {
+        plaatje3.SetActive(false);
+        knop3.SetActive(false);
+
+        plaatje4.SetActive(true);
+        knop4.SetActive(true);
     }
 }
